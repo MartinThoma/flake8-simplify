31ea7671ca174d84f30ddae377fd523fb68b3b7a
Martin Thoma <info@martin-thoma.de>
2021-11-27 15:59:24 +0100

    SIM401: Implement check if-block -> dict.get(key, default) (#73)
    Closes #72

--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -658,3 +658,45 @@ def test_sim300_int():
     assert ret == {
         "1:0 SIM300 Use 'age == 42' instead of '42 == age' (Yoda-conditions)"
     }
+
+
+def test_sim401_if_else():
+    ret = _results(
+        """if key in a_dict:
+    value = a_dict[key]
+else:
+    value = 'default'"""
+    )
+    assert ret == {
+        """1:0 SIM401 Use 'value = a_dict.get(key, "default")' """
+        """instead of an if-block"""
+    }
+
+
+def test_sim401_negated_if_else():
+    ret = _results(
+        """if key not in a_dict:
+    value = 'default'
+else:
+    value = a_dict[key] """
+    )
+    assert (
+        """1:0 SIM401 Use 'value = a_dict.get(key, "default")' """
+        """instead of an if-block""" in ret
+    )
+
+
+def test_sim401_prefix_negated_if_else():
+    ret = _results(
+        """if not key in a_dict:
+    value = 'default'
+else:
+    value = a_dict[key] """
+    )
+    assert (
+        """1:3 SIM401 Use 'value = a_dict.get(key, "default")' """
+        """instead of an if-block""" in ret
+    ) or (
+        "1:3 SIM203 Use 'key not in a_dict' instead of 'not key in a_dict'"
+        in ret
+    )
