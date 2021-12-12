bb1af75456ebfaa7acc9bd25bdfb549ecc4fcc91
Martin Thoma <info@martin-thoma.de>
2021-12-12 17:34:52 +0100

    MAINT: Run tests for Python 3.10 (#75)

--- flake8_simplify.py
+++ flake8_simplify.py
@@ -1095,28 +1095,27 @@ def _get_sim119(node: ast.ClassDef) -> List[Tuple[int, int, str]]:
     ]
     has_only_dataclass_functions = True
     has_any_functions = False
+    has_complex_statements = False
     for body_el in node.body:
         if isinstance(body_el, (ast.FunctionDef, ast.AsyncFunctionDef)):
             has_any_functions = True
             if body_el.name == "__init__":
                 # Ensure constructor only has pure assignments
                 # without any calculation.
-                has_complex_statements = False
                 for el in body_el.body:
                     if not isinstance(el, ast.Assign):
                         has_complex_statements = True
                         break
-                    else:
-                        # It is an assignment, but we only allow
-                        # `self.attribute = name`.
-                        if any(
-                            [
-                                not isinstance(target, ast.Attribute)
-                                for target in el.targets
-                            ]
-                        ) or not isinstance(el.value, ast.Name):
-                            has_complex_statements = True
-                            break
+                    # It is an assignment, but we only allow
+                    # `self.attribute = name`.
+                    if any(
+                        [
+                            not isinstance(target, ast.Attribute)
+                            for target in el.targets
+                        ]
+                    ) or not isinstance(el.value, ast.Name):
+                        has_complex_statements = True
+                        break
             if body_el.name not in dataclass_functions:
                 has_only_dataclass_functions = False
 
