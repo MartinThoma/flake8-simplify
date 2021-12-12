6eb41c99049d3b458d53c9c275b776f3cda31bd6
Danilo Šegan <danilo.segan@close.io>
2021-12-12 16:33:19 +0100

    SIM119: Adjust when it is triggered
    Make SIM119 not trigger on complex operations in the constructor, and when async methods are present on a class. (#74)

--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -480,12 +480,57 @@ class FooBar:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+"""
+    )
+    assert results == {"2:0 SIM119 Use a dataclass for 'class FooBar'"}
+
+
+def test_sim119_ignored_dunder_methods():
+    """
+    Dunder methods do not make a class not be a dataclass candidate.
+    Examples for dunder (double underscore) methods are:
+      * __str__
+      * __eq__
+      * __hash__
+    """
+    results = _results(
+        """
+class FooBar:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
     def __str__(self):
         return "FooBar"
 """
     )
     assert results == {"2:0 SIM119 Use a dataclass for 'class FooBar'"}
+
+
+def test_sim119_async():
+    results = _results(
+        """
+class FooBar:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    async def foo(self):
+        return "FooBar"
+"""
+    )
+    assert results == set()
+
+
+def test_sim119_constructor_processing():
+    results = _results(
+        """
+class FooBar:
+    def __init__(self, a):
+        self.a = a + 5
+"""
+    )
+    assert results == set()
 
 
 def test_sim119_pydantic():
