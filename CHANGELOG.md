1a55a9cc0230b9c7bdbecd5d631cbdadef91bda1
Martin Thoma <info@martin-thoma.de>
2022-02-16 19:06:18 +0100

    SIM904: Remove false-positives
    This introduces false-negatives, but that is typically better for
    a linter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 Release History
 ===============
 
+
+### 0.17.1
+Released on 16.02.2022
+
+* SIM904: Removed false-positives that happened when a dictionary value was
+          derived from another value of the same dictionary.
+
 ### 0.17.0
 Released on 13.02.2022
 
--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -937,12 +937,50 @@ def test_sim901():
     assert results == {"1:0 SIM901 Use 'a == b' instead of 'bool(a == b)'"}
 
 
-def test_sim904():
-    results = _results(
+@pytest.mark.parametrize(
+    "s",
+    (
         """a = { }
-a['b'] = 'c'"""
-    )
+a['b'] = 'c'""",
+        pytest.param(
+            """a = { }
+a['b'] = ba""",
+            marks=pytest.mark.xfail,
+        ),
+    ),
+    ids=["minimal", "contains-name"],
+)
+def test_sim904(s):
+    results = _results(s)
     assert results == {"1:0 SIM904 Initialize dictionary 'a' directly"}
+
+
+@pytest.mark.parametrize(
+    "s",
+    (
+        # Credits to MetRonnie for the following example
+        # https://github.com/MartinThoma/flake8-simplify/issues/99
+        """my_dict = {
+    'foo': [1, 2, 3, 4],
+    'bar': [5, 6, 7, 8]
+}
+my_dict['both'] = [item for _list in my_dict.values() for item in _list]""",
+        # Credits to Skylion007 for the following two examples
+        # https://github.com/MartinThoma/flake8-simplify/issues/100
+        """perf = {"total_time": end_time - start_time}
+perf["frame_time"] = perf["total_time"] / total_frames
+perf["fps"] = 1.0 / perf["frame_time"]
+perf["time_per_step"] = time_per_step
+perf["avg_sim_step_time"] = total_sim_step_time / total_frames""",
+        """perf = {"a": 1}
+perf["b"] = perf["a"] / 10""",
+    ),
+    ids=["issue-99", "issue-100-1", "issue-100-2"],
+)
+def test_sim904_false_positives(s):
+    results = _results(s)
+    for result in results:
+        assert "SIM904" not in result
 
 
 def test_sim905():
