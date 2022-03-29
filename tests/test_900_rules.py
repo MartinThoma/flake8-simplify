61d9d53ed2e69e33f023f01a690f624cf09081c9
Martin Thoma <info@martin-thoma.de>
2022-03-29 11:19:22 +0200

    SIM903: Removed due to false-positives (#131)
    Closes #130

--- tests/test_900_rules.py
+++ tests/test_900_rules.py
@@ -12,51 +12,6 @@ def test_sim901():
 
 @pytest.mark.parametrize(
     "s",
-    ("foo(a, b, 123123)", "foo(a, b, 123.123)"),
-    ids=["int", "float"],
-)
-def test_sim903_true_positive_check(s):
-    error_messages = _results(s)
-    assert any("SIM903" in error_message for error_message in error_messages)
-
-
-@pytest.mark.parametrize(
-    "s",
-    (
-        "dict.get('foo', 123)",
-        "set_foo(1.23)",
-        "line.set_foo(1.23)",
-        "partial(foo, 1, 2, 3)",
-        "min(0.5, g_norm)",
-        "QColor(53, 53, 53, 128)",
-    ),
-    ids=[
-        "get_exception",
-        "set_function",
-        "set_method",
-        "partial",
-        "min",
-        "color",
-    ],
-)
-def test_sim903_false_positive_check(s):
-    error_messages = _results(s)
-    for error_message in error_messages:
-        assert "SIM903" not in error_message
-
-
-def test_sim903_insert_exception():
-    ret = _results("sys.path.insert(0, 'foo')")
-    assert ret == set()
-
-
-def test_sim903_range_exception():
-    ret = _results("range(42)")
-    assert ret == set()
-
-
-@pytest.mark.parametrize(
-    "s",
     (
         """a = { }
 a['b'] = 'c'""",
