2a9d6c22fe3a6df0b4970a9e75cd63ceb7232347
RhinosF1 <github@rhinosf1.com>
2025-03-09 14:23:07 +0000

    constants: NameConstant was dropped in 3.14

--- flake8_simplify/constants.py
+++ flake8_simplify/constants.py
@@ -2,6 +2,10 @@
 import ast
 
 # ast.Constant in Python 3.8, ast.NameConstant in Python 3.6 and 3.7
-BOOL_CONST_TYPES = (ast.Constant, ast.NameConstant)
-AST_CONST_TYPES = (ast.Constant, ast.NameConstant, ast.Str, ast.Num)
+if hasattr(ast, 'NameConstant'):
+  BOOL_CONST_TYPES = (ast.Constant, ast.NameConstant)
+  AST_CONST_TYPES = (ast.Constant, ast.NameConstant, ast.Str, ast.Num)
+else:
+  BOOL_CONST_TYPES = (ast.Constant)
+  AST_CONST_TYPES = (ast.Constant, ast.Str, ast.Num)
 STR_TYPES = (ast.Constant, ast.Str)
