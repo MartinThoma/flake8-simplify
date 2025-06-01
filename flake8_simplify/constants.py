cd4d61aec8ba77c7b15c2379efcf34b3d372791a
Martin Thoma <info@martin-thoma.de>
2025-06-01 13:52:37 +0200

    Update flake8_simplify/constants.py

--- flake8_simplify/constants.py
+++ flake8_simplify/constants.py
@@ -7,6 +7,6 @@ if hasattr(ast, 'NameConstant'):
   AST_CONST_TYPES = (ast.Constant, ast.NameConstant, ast.Str, ast.Num)
   STR_TYPES = (ast.Constant, ast.Str)
 else:
-  BOOL_CONST_TYPES = (ast.Constant)
-  AST_CONST_TYPES = (ast.Constant)
-  STR_TYPES = (ast.Constant)
+  BOOL_CONST_TYPES = (ast.Constant,)
+  AST_CONST_TYPES = (ast.Constant,)
+  STR_TYPES = (ast.Constant,)
