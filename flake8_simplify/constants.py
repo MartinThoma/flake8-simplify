aafbec63dcf0252e84a125db0d1156d3325cd2b7
Martin Thoma <info@martin-thoma.de>
2026-01-01 00:46:06 +0100

    pre-commit

--- flake8_simplify/constants.py
+++ flake8_simplify/constants.py
@@ -3,10 +3,10 @@ import ast
 import sys
 
 if sys.version_info < (3, 8):
-  BOOL_CONST_TYPES = (ast.Constant, ast.NameConstant)
-  AST_CONST_TYPES = (ast.Constant, ast.NameConstant, ast.Str, ast.Num)
-  STR_TYPES = (ast.Constant, ast.Str)
+    BOOL_CONST_TYPES = (ast.Constant, ast.NameConstant)
+    AST_CONST_TYPES = (ast.Constant, ast.NameConstant, ast.Str, ast.Num)
+    STR_TYPES = (ast.Constant, ast.Str)
 else:
-  BOOL_CONST_TYPES = (ast.Constant,)
-  AST_CONST_TYPES = (ast.Constant,)
-  STR_TYPES = (ast.Constant,)
+    BOOL_CONST_TYPES = (ast.Constant,)
+    AST_CONST_TYPES = (ast.Constant,)
+    STR_TYPES = (ast.Constant,)
